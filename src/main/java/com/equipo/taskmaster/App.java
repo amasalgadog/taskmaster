cac31b5283d4dbdd4bdf87c8fa2ddc9d18902db7
gato4d <alvaro.asalgado@ug.uchile.cl>
2025-06-12 00:05:25 -0400

    agrego ejercicio completo de maven

---
+++ src/main/java/com/equipo/taskmaster/App.java
@@ -0,0 +1,31 @@
+package com.equipo.taskmaster;
+
+import java.util.ArrayList;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static ArrayList<String> tasks = new ArrayList<>();
+    
+    public static void main( String[] args )
+    {
+        System.out.println( "Bienvenido a TaskMaster!" );
+        addTask("Estudiar Maven");
+        addTask("Leer sobre CI/CD");
+        System.out.println("Ambiente: " + System.getProperty("env.name"));
+        printTasks();
+    }
+
+    public static void addTask(String task){
+        tasks.add(task);
+    }
+
+    public static void printTasks(){
+        System.out.println("Tareas pendientes:");
+        for (String t: tasks){
+            System.out.println("- " + t);
+        }
+    }
+}
